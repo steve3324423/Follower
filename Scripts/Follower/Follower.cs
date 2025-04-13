5defa649fed68db4e4684985e872370a3738cbc2
steve3324423 <64463182+steve3324423@users.noreply.github.com>
2025-04-13 16:32:42 +0300

    Update Follower.cs

--- Scripts/Follower/Follower.cs
+++ Scripts/Follower/Follower.cs
@@ -1,15 +1,29 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Follower : MonoBehaviour
 {
     [SerializeField] private Player _player;
 
-    private float _xPositionOffset = 3f;
-    private float _speed = 5f;
+    private Rigidbody _rigidbody;
+    private float _minDistance = 1.5f;
+    private float _speed = 2f;
 
-    private void Update()
+    private void Awake()
     {
-        Vector3 targetPosition = new Vector3(_player.transform.position.x + _xPositionOffset,1f,_player.transform.position.z);
-        transform.position = Vector3.MoveTowards(transform.position,targetPosition,_speed * Time.deltaTime);
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        TryGoToPlayer();
+    }
+
+    private void TryGoToPlayer()
+    {
+        Vector3 direction = _player.transform.position - transform.position;
+
+        if(direction.sqrMagnitude > _minDistance * _minDistance)
+            _rigidbody.MovePosition(transform.position + (direction * _speed * Time.fixedDeltaTime));
     }
 }
